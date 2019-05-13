b485598342667237f45147c7430c104753e11898
moshea <moshea@gett.com>
2019-05-13 12:57:27 +0300

    Working reducer with show and delete

--- src/components/TestResult.jsx
+++ src/components/TestResult.jsx
@@ -1,17 +1,53 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import { Consumer } from '../context'
 
-function TestResult(props) {
-  const { status, name, reason } = props.result;
-  return (
-    <div className="card card-body mb-3">
-      <h4>Result: {status}</h4>
-        <ul className="list-group">
-            <li className="list-group-item">Name: {name}</li>
-            <li className="list-group-item">Reason: {reason}</li>
-        </ul>
-    </div>
-  )
+class TestResult extends Component {
+  state = {
+    showTestResult: false
+  };
+
+  onDeleteClick = (id, dispatch) => {
+    dispatch({ type: 'DELETE_TEST_RESULT', payload: id });
+  };
+
+  render() {
+    const { showTestResult } = this.state;
+    const { id, status, name, reason } = this.props.result;
+
+    return (
+      <Consumer>
+        {value => {
+          const { dispatch } = value;
+          return (
+            <div className="card card-body mb-3">
+              <h4>Result: {status}{' '}
+                <i
+                  onClick={() =>
+                    this.setState({
+                      showTestResult: !this.state.showTestResult
+                    })
+                  }
+                  className="fas fa-sort-down"
+                  style={{ cursor: 'pointer' }}
+                />
+                <i
+                  className="fas fa-times"
+                  style={{ cursor: 'pointer', float: 'right', color: 'red' }}
+                  onClick={this.onDeleteClick.bind(this, id, dispatch)}
+                />
+                {showTestResult ? (<ul className="list-group">
+                  <li className="list-group-item">Name: {name}</li>
+                  <li className="list-group-item">Reason: {reason}</li>
+                </ul>) : null
+                }
+              </h4>
+            </div>
+          );
+        }}
+      </Consumer>
+    )
+  };
 }
 
 TestResult.propTypes = {
--- src/context.js
+++ src/context.js
@@ -2,6 +2,20 @@ import React, { Component } from 'react'
 
 const Context = React.createContext();
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'DELETE_TEST_RESULT':
+      return {
+        ...state,
+        testResults: state.testResults.filter(tr => tr.id !== action.payload)
+      }
+    default:
+      return {
+        ...state
+      }
+  }
+};
+
 export class Provider extends Component {
   state = {
     testResults: [
@@ -23,7 +37,10 @@ export class Provider extends Component {
         status: "inconclusive",
         reason: "env problem"
       }
-    ]
+    ],
+    dispatch: action => {
+      this.setState(state => reducer(state, action));
+    }
   }
 
   render() {
